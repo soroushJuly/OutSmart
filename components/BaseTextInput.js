702cfe089f707ebf946b785601fba5d6f95acac3
Soroush <soroushdevelope@gmail.com>
2025-03-04 23:54:08 +0000

    fix: tiny bug in base text input

--- components/BaseTextInput.js
+++ components/BaseTextInput.js
@@ -1,11 +1,11 @@
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
 
 
-function BaseTextInput({ value, placeholder, label, onChange, isSensitive, style }) {
+function BaseTextInput({ value, placeholder, label, onChangeText, isSensitive, style }) {
     return (
         <View style={[style]}>
             <Text style={styles.label}>{label}</Text>
-            <TextInput value={value} placeholder={placeholder} style={styles.input} onChange={onChange} secureTextEntry={isSensitive} />
+            <TextInput value={value} placeholder={placeholder} style={styles.input} onChangeText={onChangeText} secureTextEntry={isSensitive} />
         </View>
     )
 }
