52d175e51f48aca648f15a06fcdb7bfd68681eb5
Soroush <soroushdevelope@gmail.com>
2025-03-05 00:34:59 +0000

    chore: improve login logic

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,27 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import BaseTextInput from '../components/BaseTextInput';
 import BaseButton from '../components/BaseButton';
 import { useDispatch } from 'react-redux';
 import { updateAuth } from '../store/authSlice';
 import { loginUser } from '../utils/api';
+import * as SecureStore from 'expo-secure-store';
 
 
 function LoginScreen({ navigation }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('ali@test.comm');
+    const [password, setPassword] = useState('12345678aA@');
+    const [hasError, setHasError] = useState(false);
 
-    function handleUsername(value) {
+    function handleUsernameChange(value) {
         setUsername(value);
     }
     function handlePassword(value) {
         setPassword(value);
     }
 
+    useEffect(() => {
+        console.log('login screen mounted');
+    }, []);
+
     async function handleLogin() {
-        const data = await loginUser({ username: 'sa', password: 'g' });
-        console.log(data);
-        // dispatch(updateAuth());
+        // const token = await SecureStore.getItemAsync('secure_token');
+        const { data, success } = await loginUser({ username, password });
+
+        if (success === false) {
+            setHasError(true);
+            return;
+        }
+
+        // save token to local storage
+        await SecureStore.setItemAsync('secure_token', data.token);
+        console.log('token:', token);
+        // navigate to home screen
         navigation.navigate('Tabs');
     }
 
@@ -34,9 +49,10 @@ function LoginScreen({ navigation }) {
         <View style={styles.screenContainer}>
             <Text style={styles.h1}>Login</Text>
             <View style={styles.inputContainer}>
-                <BaseTextInput label="Username" value={username} placeholder='Username' onChange={handleUsername} style={{ marginBottom: 10 }} />
+                <BaseTextInput label="Username" value={username} placeholder='Username' onChangeText={handleUsernameChange} style={{ marginBottom: 10 }} />
                 <BaseTextInput label="Password" value={password} placeholder='password' onChange={handlePassword} isSensitive={true} />
             </View>
+            {hasError && <Text style={{ color: 'red' }}>Invalid username or password</Text>}
             <BaseButton title="Login" onPress={handleLogin} style={{ marginBottom: 10 }} />
             <BaseButton title="Sign Up" onPress={goToSignUp} />
         </View>
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../constants/urls";
+import { LOGIN_URL } from "../constants/endpoints";
 
 export function PostSignUp({ username, password }) {
     axios.post(BASE_URL + 'users.json', { username, password })
@@ -8,14 +8,25 @@ export function PostSignUp({ username, password }) {
 export function FetchUsers() {
     axios.get(BASE_URL + 'users.json')
 }
+// ------------ STATUS CHECKER ---------- //
 
 
 // ------------ AUTH ---------- //
 export async function loginUser({ username, password }) {
-    const response = await axios.post(BASE_URL + 'users.json', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: { username, password },
-    });
-    return response;
+    try {
+        const response = await axios.post(LOGIN_URL, {
+            email: username,
+            password
+        });
+
+        const data = response.data.data;
+        const success = response.data.success;
+        const message = response.data.message;
+        return { success, data };
+    } catch (error) {
+        const data = error.response?.data?.data;
+        const success = error.response.data.success;
+        const message = error.response.data.message;
+        return { success, message, data };
+    }
 };
