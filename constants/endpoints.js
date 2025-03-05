11fc5f95d88367baeb347dc5d8f6416902674081
Soroush <soroushdevelope@gmail.com>
2025-03-05 00:25:59 +0000

    add: endpoints

---
+++ constants/endpoints.js
@@ -0,0 +1,3 @@
+export const BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
+export const AUTH_URL = BASE_URL + '/auth';
+export const LOGIN_URL = AUTH_URL + '/login';
--- constants/urls.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'https://react-native-test-253c4-default-rtdb.firebaseio.com/'
