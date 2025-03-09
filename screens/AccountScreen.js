50762feb4dd8404140a53b30da31db6f9d193e0d
Soroush <soroushdevelope@gmail.com>
2025-03-09 18:55:45 +0000

    Initilize accout screen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,5 +1,35 @@
-function AccountScreen() {
+import { View, Text, Image, StyleSheet } from 'react-native'
+import BaseButton from '../components/BaseButton';
 
+function AccountScreen() {
+    return (
+        <View style={{ flex: 1, padding: 16 }}>
+            <View style={{ flexDirection: 'row', marginBottom: 24 }}>
+                <View>
+                    <Image style={{ width: "100", height: '100', borderRadius: 10 }} source={require("../assets/icon.png")} width="100" height="100" />
+                </View>
+                <View style={{ justifyContent: 'space-around', marginLeft: 8 }}>
+                    <Text style={styles.profileText}>Username: </Text>
+                    <Text style={styles.profileText}>Credit: </Text>
+                    <Text style={styles.profileText}>Wins:</Text>
+                </View>
+            </View>
+            <BaseButton title="Cashout" style={styles.button} />
+            <BaseButton title="Change Email" style={styles.button} />
+            <BaseButton title="Change Password" style={styles.button} />
+            <BaseButton title="FAQs" style={styles.button} />
+            <BaseButton title="Logout" style={styles.button} />
+            {/* <Text></Text> */}
+        </View>
+    )
 }
 
 export default AccountScreen;
+
+const styles = StyleSheet.create({
+    button: {
+        textAlign: 'left',
+        marginBottom: 12
+    },
+    profileText: { fontSize: 20 }
+})
